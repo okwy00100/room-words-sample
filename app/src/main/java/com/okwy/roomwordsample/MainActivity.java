41224a9437e10fc33e88dd896864c10915bf3479
oknwachukwu <okwy.m.nwachukwu@gmail.com>
2020-09-14 20:08:44 +0100

    Added "Delete Word" feature, Swipe to delete. Work on New Word Activity

--- app/src/main/java/com/okwy/roomwordsample/MainActivity.java
+++ app/src/main/java/com/okwy/roomwordsample/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -60,6 +61,24 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(startIntent, NEW_WORD_ACTIVITY_REQUEST_CODE);
             }
         });
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();
+                Word myWord = adapter.getWordAtPosition(position);
+                Toast.makeText(MainActivity.this, "Deleting " +
+                        myWord.getWord(), Toast.LENGTH_LONG).show();
+                mWordViewModel.deleteWord(myWord);
+            }
+        });
+
+        itemTouchHelper.attachToRecyclerView(recyclerView);
     }
 
     @Override
